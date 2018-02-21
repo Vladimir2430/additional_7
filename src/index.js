5eccae169a62442bf7a16535ee60e6bb87e4a728
Uladzimir <berkut2430@gmail.com>
2018-02-21 19:40:42 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,20 +13,20 @@ module.exports = function solveSudoku(matrix) {
   }
   var arr=[arr1, arr2, arr3, arr4, arr5, arr6, arr7, arr8, arr9];
 
-  for (let i=0; i<9; ++i) {
-    for (let k=0; k<9;k++) {
-      if (matrix[0][i]==0) {
-        if (matrix[k].indexOf(1)==-1 && arr[i].indexOf(1)==-1) {martix[k][i]=1;}
-        if (matrix[k].indexOf(2)==-1 && arr[i].indexOf(2)==-1) {martix[k][i]=2;}
-        if (matrix[k].indexOf(3)==-1 && arr[i].indexOf(3)==-1) {martix[k][i]=3;}
-        if (matrix[k].indexOf(4)==-1 && arr[i].indexOf(4)==-1) {martix[k][i]=4;}
-        if (matrix[k].indexOf(5)==-1 && arr[i].indexOf(5)==-1) {martix[k][i]=5;}
-        if (matrix[k].indexOf(6)==-1 && arr[i].indexOf(6)==-1) {martix[k][i]=6;}
-        if (matrix[k].indexOf(7)==-1 && arr[i].indexOf(7)==-1) {martix[k][i]=7;}
-        if (matrix[k].indexOf(8)==-1 && arr[i].indexOf(8)==-1) {martix[k][i]=8;}
-        if (matrix[k].indexOf(9)==-1 && arr[i].indexOf(9)==-1) {martix[k][i]=9;}
-      }
+  var array=matrix;
+
+  for (let i=0; i<9; i++) {
+    if (matrix[0][i]==0) {
+      if (matrix[0].indexOf(1)==-1 && arr[i].indexOf(1)==-1) {array[0][i]=1;}
+      if (matrix[0].indexOf(2)==-1 && arr[i].indexOf(2)==-1) {array[0][i]=2;}
+      if (matrix[0].indexOf(3)==-1 && arr[i].indexOf(3)==-1) {array[0][i]=3;}
+      if (matrix[0].indexOf(4)==-1 && arr[i].indexOf(4)==-1) {array[0][i]=4;}
+      if (matrix[0].indexOf(5)==-1 && arr[i].indexOf(5)==-1) {array[0][i]=5;}
+      if (matrix[0].indexOf(6)==-1 && arr[i].indexOf(6)==-1) {array[0][i]=6;}
+      if (matrix[0].indexOf(7)==-1 && arr[i].indexOf(7)==-1) {array[0][i]=7;}
+      if (matrix[0].indexOf(8)==-1 && arr[i].indexOf(8)==-1) {array[0][i]=8;}
+      if (matrix[0].indexOf(9)==-1 && arr[i].indexOf(9)==-1) {array[0][i]=9;}
     }
   }
-  return matrix;
+  return array;
 }
