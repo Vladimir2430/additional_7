69d1699fa3908fe76015f4a3ce62b9f8bff38439
Uladzimir <uladzimir.yeudakimovich@gmail.com>
2018-07-23 20:53:13 +0300

    code improved

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ module.exports = function solveSudoku(matrix) {
 }
 
 function matrixFunction(matrix, arr) {
-  for(let i=arr[0]; i<9; i++) {
-    for(let j=arr[1]; j<9; j++) {
-      if(matrix[i][j]==0){
-        arr[0]=i;
-        arr[1]=j;
+  for(let i = arr[0]; i < 9; i++) {
+    for(let j = arr[1]; j < 9; j++) {
+      if (matrix[i][j] === 0){
+        arr[0] = i;
+        arr[1] = j;
         return 1;
       }
     }
@@ -17,35 +17,37 @@ function matrixFunction(matrix, arr) {
 }
 
 function rowFunction(matrix, row, num) {
-  for(let i=0; i<9; i++)
-    if(matrix[row][i]==num) {return 1;}
+  for(let i = 0; i < 9; i++) {
+    if(matrix[row][i] === num) { return 1; }
+  }
   return 0;
 }
 
 function colFunction(matrix, col, num) {
-  for(let i=0; i<9; i++)
-    if(matrix[i][col]==num) {return 1;}
+  for(let i = 0; i < 9; i++) {
+    if(matrix[i][col] === num) { return 1; }
+  }
   return 0;
 }
 
 function boxFunction(matrix, row, col, num) {
-  var arrRow=(Math.floor(row/3))*3;
-  var arrCol=(Math.floor(col/3))*3;
-  for(let i=arrRow; i<arrRow+3; i++)
-    for(let j=arrCol; j<arrCol+3; j++)
-      if(matrix[i][j]==num) {return 1;}
+  let arrRow = (Math.floor(row / 3)) * 3, arrCol = (Math.floor(col / 3)) * 3;
+  for(let i = arrRow; i < arrRow + 3; i++)
+    for(let j = arrCol; j < arrCol + 3; j++) {
+      if(matrix[i][j] === num) { return 1; }
+    }
   return 0;
 }
 
 function sudokuFunction(matrix){
-  var arr=[0, 0];
-  if (!matrixFunction(matrix, arr)) {return 1;}
-  var row=arr[0], col=arr[1];
-  for (let i=1; i<=9; i++) {
+  let arr=[0, 0];
+  if (!matrixFunction(matrix, arr)) { return 1; }
+  let row = arr[0], col = arr[1];
+  for (let i = 1; i <= 9; i++) {
     if (!rowFunction(matrix, row, i) && !colFunction(matrix, col, i) && !boxFunction(matrix, row, col, i)) {
-      matrix[row][col]=i;
-      if (sudokuFunction(matrix)) {return 1;}
-      matrix[row][col]=0;
+      matrix[row][col] = i;
+      if (sudokuFunction(matrix)) { return 1; }
+      matrix[row][col] = 0;
     }
   }
   return 0;
